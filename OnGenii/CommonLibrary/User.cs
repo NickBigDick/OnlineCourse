aa7751d175a67ea723dd35e458f2f25088fa456a
@nikolai_tevosian <kolya0714@yandex.ru>
2023-10-27 18:30:04 +0300

    Написал winform

---
+++ OnGenii/CommonLibrary/User.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CommonLibrary
+{
+    public class User
+    {
+        public string Name { get; }
+        public string Diagnose { get; set; }
+        public UserResultStorage Storage { get; }
+
+        public void AddResult(string result)
+        {
+            Storage.Results.Add(result);
+        }
+
+        public void ShowResults()
+        {
+            foreach (string result in Storage.Results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+
+        public User(string name)
+        {
+            Name = name;
+            Storage = new UserResultStorage(this);
+
+        }
+
+        public void SetDiagnose(int rightAnswers, int totalQuestions)
+        {
+            int index;
+            string[] diagnoses = new string[6];
+            diagnoses[0] = "кретин";
+            diagnoses[1] = "идиот";
+            diagnoses[2] = "дурак";
+            diagnoses[3] = "нормальный";
+            diagnoses[4] = "талант";
+            diagnoses[5] = "гении";
+            if (rightAnswers > 0)
+            {
+
+                double percentage = (double)rightAnswers / totalQuestions;
+                index = (int)Math.Round(percentage * 6.0) - 1;
+            }
+            else
+            {
+                index = 0;
+            }
+            Diagnose = diagnoses[index];
+
+        }
+    }
+}
---
+++ OnGenii/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.userAnswerTextBox = new System.Windows.Forms.TextBox();
+            this.questionNumberLabel = new System.Windows.Forms.Label();
+            this.questionTextLabel = new System.Windows.Forms.Label();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newgameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showresultsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nextButton
+            // 
+            this.nextButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nextButton.Location = new System.Drawing.Point(104, 229);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(100, 53);
+            this.nextButton.TabIndex = 0;
+            this.nextButton.Text = "Далее";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // userAnswerTextBox
+            // 
+            this.userAnswerTextBox.Location = new System.Drawing.Point(12, 169);
+            this.userAnswerTextBox.Name = "userAnswerTextBox";
+            this.userAnswerTextBox.Size = new System.Drawing.Size(171, 20);
+            this.userAnswerTextBox.TabIndex = 1;
+            // 
+            // questionNumberLabel
+            // 
+            this.questionNumberLabel.AutoSize = true;
+            this.questionNumberLabel.Location = new System.Drawing.Point(12, 67);
+            this.questionNumberLabel.Name = "questionNumberLabel";
+            this.questionNumberLabel.Size = new System.Drawing.Size(64, 13);
+            this.questionNumberLabel.TabIndex = 2;
+            this.questionNumberLabel.Text = "Вопрос №1";
+            // 
+            // questionTextLabel
+            // 
+            this.questionTextLabel.AutoSize = true;
+            this.questionTextLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.questionTextLabel.Location = new System.Drawing.Point(12, 92);
+            this.questionTextLabel.Name = "questionTextLabel";
+            this.questionTextLabel.Size = new System.Drawing.Size(118, 20);
+            this.questionTextLabel.TabIndex = 3;
+            this.questionTextLabel.Text = "Текст вопроса";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(447, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileStripMenuItem
+            // 
+            this.fileStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newgameToolStripMenuItem,
+            this.showresultsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileStripMenuItem.Name = "fileStripMenuItem";
+            this.fileStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.fileStripMenuItem.Text = "Файл";
+            // 
+            // newgameToolStripMenuItem
+            // 
+            this.newgameToolStripMenuItem.Name = "newgameToolStripMenuItem";
+            this.newgameToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.newgameToolStripMenuItem.Text = "Новая игра";
+            this.newgameToolStripMenuItem.Click += new System.EventHandler(this.newgameToolStripMenuItem_Click);
+            // 
+            // showresultsToolStripMenuItem
+            // 
+            this.showresultsToolStripMenuItem.Name = "showresultsToolStripMenuItem";
+            this.showresultsToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.showresultsToolStripMenuItem.Text = "Показать результаты";
+            this.showresultsToolStripMenuItem.Click += new System.EventHandler(this.showresultsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.exitToolStripMenuItem.Text = "Выход";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 403);
+            this.Controls.Add(this.questionTextLabel);
+            this.Controls.Add(this.questionNumberLabel);
+            this.Controls.Add(this.userAnswerTextBox);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.TextBox userAnswerTextBox;
+        private System.Windows.Forms.Label questionNumberLabel;
+        private System.Windows.Forms.Label questionTextLabel;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newgameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showresultsToolStripMenuItem;
+    }
+}
+
