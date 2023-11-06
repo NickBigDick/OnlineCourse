3792d7aa37374b64297d38d892f6eec96096b234
@nikolai_tevosian <kolya0714@yandex.ru>
2023-11-06 11:31:16 +0300

    +

---
+++ FrogWinFromsApp/FrogWinFromsApp/MainForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace FrogWinFromsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.leftPictureBox1 = new System.Windows.Forms.PictureBox();
+            this.leftPictureBox2 = new System.Windows.Forms.PictureBox();
+            this.leftPictureBox3 = new System.Windows.Forms.PictureBox();
+            this.leftPictureBox4 = new System.Windows.Forms.PictureBox();
+            this.rightPictureBox4 = new System.Windows.Forms.PictureBox();
+            this.rightPictureBox3 = new System.Windows.Forms.PictureBox();
+            this.rightPictureBox2 = new System.Windows.Forms.PictureBox();
+            this.rightPictureBox1 = new System.Windows.Forms.PictureBox();
+            this.emptyPictureBox = new System.Windows.Forms.PictureBox();
+            this.counterLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptyPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // leftPictureBox1
+            // 
+            this.leftPictureBox1.Image = global::FrogWinFromsApp.Properties.Resources._115450401_83e0c400_a224_11eb_9318_bd1f8db58b52;
+            this.leftPictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.leftPictureBox1.Name = "leftPictureBox1";
+            this.leftPictureBox1.Size = new System.Drawing.Size(104, 110);
+            this.leftPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.leftPictureBox1.TabIndex = 0;
+            this.leftPictureBox1.TabStop = false;
+            this.leftPictureBox1.Click += new System.EventHandler(this.rightPictureBox_Click);
+            // 
+            // leftPictureBox2
+            // 
+            this.leftPictureBox2.Image = global::FrogWinFromsApp.Properties.Resources._115450401_83e0c400_a224_11eb_9318_bd1f8db58b52;
+            this.leftPictureBox2.Location = new System.Drawing.Point(104, 0);
+            this.leftPictureBox2.Name = "leftPictureBox2";
+            this.leftPictureBox2.Size = new System.Drawing.Size(104, 110);
+            this.leftPictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.leftPictureBox2.TabIndex = 0;
+            this.leftPictureBox2.TabStop = false;
+            this.leftPictureBox2.Click += new System.EventHandler(this.rightPictureBox_Click);
+            // 
+            // leftPictureBox3
+            // 
+            this.leftPictureBox3.Image = global::FrogWinFromsApp.Properties.Resources._115450401_83e0c400_a224_11eb_9318_bd1f8db58b52;
+            this.leftPictureBox3.Location = new System.Drawing.Point(208, 0);
+            this.leftPictureBox3.Name = "leftPictureBox3";
+            this.leftPictureBox3.Size = new System.Drawing.Size(104, 110);
+            this.leftPictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.leftPictureBox3.TabIndex = 0;
+            this.leftPictureBox3.TabStop = false;
+            this.leftPictureBox3.Click += new System.EventHandler(this.rightPictureBox_Click);
+            // 
+            // leftPictureBox4
+            // 
+            this.leftPictureBox4.Image = global::FrogWinFromsApp.Properties.Resources._115450401_83e0c400_a224_11eb_9318_bd1f8db58b52;
+            this.leftPictureBox4.Location = new System.Drawing.Point(312, 0);
+            this.leftPictureBox4.Name = "leftPictureBox4";
+            this.leftPictureBox4.Size = new System.Drawing.Size(104, 110);
+            this.leftPictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.leftPictureBox4.TabIndex = 0;
+            this.leftPictureBox4.TabStop = false;
+            this.leftPictureBox4.Click += new System.EventHandler(this.rightPictureBox_Click);
+            // 
+            // rightPictureBox4
+            // 
+            this.rightPictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("rightPictureBox4.Image")));
+            this.rightPictureBox4.Location = new System.Drawing.Point(832, 0);
+            this.rightPictureBox4.Name = "rightPictureBox4";
+            this.rightPictureBox4.Size = new System.Drawing.Size(104, 110);
+            this.rightPictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.rightPictureBox4.TabIndex = 1;
+            this.rightPictureBox4.TabStop = false;
+            this.rightPictureBox4.Click += new System.EventHandler(this.rightPictureBox_Click);
+            // 
+            // rightPictureBox3
+            // 
+            this.rightPictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("rightPictureBox3.Image")));
+            this.rightPictureBox3.Location = new System.Drawing.Point(728, 0);
+            this.rightPictureBox3.Name = "rightPictureBox3";
+            this.rightPictureBox3.Size = new System.Drawing.Size(104, 110);
+            this.rightPictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.rightPictureBox3.TabIndex = 2;
+            this.rightPictureBox3.TabStop = false;
+            this.rightPictureBox3.Click += new System.EventHandler(this.rightPictureBox_Click);
+            // 
+            // rightPictureBox2
+            // 
+            this.rightPictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("rightPictureBox2.Image")));
+            this.rightPictureBox2.Location = new System.Drawing.Point(624, 0);
+            this.rightPictureBox2.Name = "rightPictureBox2";
+            this.rightPictureBox2.Size = new System.Drawing.Size(104, 110);
+            this.rightPictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.rightPictureBox2.TabIndex = 3;
+            this.rightPictureBox2.TabStop = false;
+            this.rightPictureBox2.Click += new System.EventHandler(this.rightPictureBox_Click);
+            // 
+            // rightPictureBox1
+            // 
+            this.rightPictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("rightPictureBox1.Image")));
+            this.rightPictureBox1.Location = new System.Drawing.Point(520, 0);
+            this.rightPictureBox1.Name = "rightPictureBox1";
+            this.rightPictureBox1.Size = new System.Drawing.Size(104, 110);
+            this.rightPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.rightPictureBox1.TabIndex = 4;
+            this.rightPictureBox1.TabStop = false;
+            this.rightPictureBox1.Click += new System.EventHandler(this.rightPictureBox_Click);
+            // 
+            // emptyPictureBox
+            // 
+            this.emptyPictureBox.Image = global::FrogWinFromsApp.Properties.Resources._115450431_89d6a500_a224_11eb_9b39_55383aa4a7de;
+            this.emptyPictureBox.Location = new System.Drawing.Point(416, 0);
+            this.emptyPictureBox.Name = "emptyPictureBox";
+            this.emptyPictureBox.Size = new System.Drawing.Size(104, 110);
+            this.emptyPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.emptyPictureBox.TabIndex = 5;
+            this.emptyPictureBox.TabStop = false;
+            // 
+            // counterLabel
+            // 
+            this.counterLabel.AutoSize = true;
+            this.counterLabel.Location = new System.Drawing.Point(231, 131);
+            this.counterLabel.Name = "counterLabel";
+            this.counterLabel.Size = new System.Drawing.Size(115, 13);
+            this.counterLabel.TabIndex = 6;
+            this.counterLabel.Text = "Количество шагов - 0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(936, 199);
+            this.Controls.Add(this.counterLabel);
+            this.Controls.Add(this.emptyPictureBox);
+            this.Controls.Add(this.rightPictureBox4);
+            this.Controls.Add(this.rightPictureBox3);
+            this.Controls.Add(this.rightPictureBox2);
+            this.Controls.Add(this.rightPictureBox1);
+            this.Controls.Add(this.leftPictureBox4);
+            this.Controls.Add(this.leftPictureBox3);
+            this.Controls.Add(this.leftPictureBox2);
+            this.Controls.Add(this.leftPictureBox1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptyPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox leftPictureBox1;
+        private System.Windows.Forms.PictureBox leftPictureBox2;
+        private System.Windows.Forms.PictureBox leftPictureBox3;
+        private System.Windows.Forms.PictureBox leftPictureBox4;
+        private System.Windows.Forms.PictureBox rightPictureBox4;
+        private System.Windows.Forms.PictureBox rightPictureBox3;
+        private System.Windows.Forms.PictureBox rightPictureBox2;
+        private System.Windows.Forms.PictureBox rightPictureBox1;
+        private System.Windows.Forms.PictureBox emptyPictureBox;
+        private System.Windows.Forms.Label counterLabel;
+    }
+}
+
